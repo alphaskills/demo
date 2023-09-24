aaf870a5d2a6888e41b9c012702637527541dc3f
alphaskills <learnskillwithsam@gmail.com>
2023-09-23 20:51:52 -0400

    first commit

---
+++ src/main/java/com/example/demo/EmployeeClient.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import com.example.demo.vo.Employee;
+
+public class EmployeeClient {
+    public void buildEmployee(){
+        Employee e = Employee.builder().build();
+       new Employee();
+    }
+}
